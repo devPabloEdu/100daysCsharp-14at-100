a27f58b6c911f05f14f74362a0f344a6d5355149
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-07-03 20:46:12 -0300

    arrumando o repositorio

---
+++ day14/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace daysCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            SearchName(); 
+        }
+
+        static void SearchName()
+        {
+            var nameList = new string[] {"Romarinho", "Cassio", "Tupanzinho", "Edilson", "Ronaldo"};
+            var correctName = "Ronaldo";
+            var found = false;
+
+            for (int i = 0; i < nameList.Length; i++)
+            {
+                if (correctName == nameList[i])
+                {
+                    Console.WriteLine($"O nome {correctName}, foi encontrado na lista na posição {i}.");
+                    found = true;
+                    break; // Sai do loop assim que o nome é encontrado.
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("O nome não existe na lista!");
+            }
+        }
+    }
+}
