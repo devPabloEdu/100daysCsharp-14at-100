ff0de966c291407cab0fa43683135e416e6dd8da
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-08-03 12:47:27 -0300

    corrigindo kkkkk

--- day17/Program.cs
+++ day17/Program.cs
@@ -3,25 +3,14 @@ class InsertionSortExample
 {
     static void Main()
     {
-        int[] lista = { 64, 25, 12, 22, 11 };
-         Console.WriteLine("Array original:");
-        PrintArray(array);
-
-        InsertionSort(array);
-
-        Console.WriteLine("\nArray ordenado:");
-        PrintArray(array);
+        int[] array = { 64, 25, 12, 22, 11 };
     }
-    static void InsertionSort(int lista){
-        var n = Length(int lista);
-        for i in range(1, n){
-            chave = int lista[i];
-            var j = i - 1;
-            while j >= 0 && int lista{j} > chave{
-                int lista[j+1] = int lista[j];
-                j = j - 1;
-            }
-            int lista[j+1] = chave;
+    static void InsertionSort(int[] array)
+    {
+        int size = array.Length;
+        for(int i = 1; i < size; i++)
+        {
+            
         }
     }
 }
