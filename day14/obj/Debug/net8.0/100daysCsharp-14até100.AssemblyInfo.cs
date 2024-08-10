ca1ce6ada28910e7e39879fed9dd8939bd712c8a
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-08-09 22:08:00 -0300

    Classe tree, e seus metodos

--- day14/obj/Debug/net8.0/100daysCsharp-14até100.AssemblyInfo.cs
+++ day14/obj/Debug/net8.0/100daysCsharp-14até100.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("100daysCsharp-14até100")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cce7695bb83ce56cedfbcd6bd9f9f7515a29f87b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfb87ced7776f439187f448f619e5078f49f721b")]
 [assembly: System.Reflection.AssemblyProductAttribute("100daysCsharp-14até100")]
 [assembly: System.Reflection.AssemblyTitleAttribute("100daysCsharp-14até100")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- day21/Program.cs
+++ day21/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ArvoreBinaria
+{
+    static void Main(string[] args)
+    {
+    
+    }
+
+
+
+    public class Tree
+    {
+        public Node Root {get; set;};
+        public class Node
+    {
+        public int Value{get; set;};
+        public Node Left{get; set;};
+        public Node Right{get; set;};
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+
+        public bool IsLeaf(){
+            return left == null && right == null;
+        }
+    }
+
+    }
+
+
+
+}
