52855c581c10a7b84d7c5577736950574a4d3a6d
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-08-20 20:35:25 -0300

    dia 26 - Propriedades

--- day26/Program.cs
+++ day26/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace day26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var pagamento = new Pagamento(); 
+           pagamento.DataPagamento = DateTime.Now;
+           Console.WriteLine(pagamento.DataPagamento.ToString());
+        }
+
+    }
+
+    public class Pagamento
+    {
+        public DateTime Vencimento { get; set; }
+
+        private DateTime _dataPagamento;
+        public DateTime DataPagamento
+        {
+            get { 
+                Console.WriteLine("---Lendo um valor---");
+                return _dataPagamento; 
+                }
+
+            set {
+                 Console.WriteLine("___Atribuindo um valor___");   
+                 _dataPagamento = value; 
+                 }
+        }
+        
+    }
+}
