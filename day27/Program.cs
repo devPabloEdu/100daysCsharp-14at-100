7d86365b2cb7336a991deb5f1864b6d191c0b47a
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-08-21 21:17:40 -0300

    dia27 - metodos e sobscrever

--- day27/Program.cs
+++ day27/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using  System;
+namespace day27
+{
+   class Program
+   {
+    static void Main(string[] args)
+    {
+        var pagamento = new PagamentoCartao();
+        pagamento.Pagar("123");
+
+        Console.WriteLine("----------");
+
+        var pagamento2 = new Pagamento();
+        pagamento2.Pagar("123");
+    }
+   } 
+   public class Pagamento
+   { 
+    public Pagamento()
+    {
+        Console.WriteLine("Iniciando o Pagamento");
+    }
+    public virtual void Pagar(string numeroDoBoleto)
+    {
+        Console.WriteLine("Pagar com boleto");
+    }
+   }
+   public class PagamentoCartao : Pagamento
+   {
+    public override void Pagar(string numeroDoCartão)
+    {
+        Console.WriteLine("Pagar com cartão.");
+    }
+   }
+}
