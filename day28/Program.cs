84e2806020dace4ae8cf63ca4298742bc9afe41a
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-08-22 20:36:43 -0300

    dia28 - using e dispose

--- day28/Program.cs
+++ day28/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace day28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var pagamento = new Pagamento())
+            {
+                Console.WriteLine("Processando o pagamento");
+            }
+        }
+    }
+
+    public class Pagamento : IDisposable
+    {
+        public Pagamento()
+        {
+            Console.WriteLine("iniciando o pagamento");
+        }
+
+        public void Dispose()
+        {
+           Console.WriteLine("Finalizando o pagamento");
+        }
+    }
+}
