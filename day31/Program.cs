2026eb557d1e5678480a4581f1aae9553ef699b2
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-08-28 21:08:28 -0300

    day31 - Classes Abstratas & Interfaces

--- day31/Program.cs
+++ day31/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Day31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            var pagamentoApple = new PagamentoApplePay();
+            pagamentoApple.Pagar(20.00);
+        }
+    }
+
+    public interface IPayment
+    {
+        DateTime Vencimento { get; set; }
+        void Pagar(double valor);
+    }
+
+    public abstract class PagamentoAbstrato : IPayment
+    {
+        public DateTime Vencimento { get; set; }
+
+        public virtual void Pagar(double valor)
+        {
+            
+        }
+    }
+
+    public class PagamentoApplePay : PagamentoAbstrato
+    {
+        public override void Pagar(double valor)
+        {
+            Console.WriteLine("Pago via Apple Pay");
+        }
+    }
+
+    public class PaymentCreditCard : IPayment
+    {
+        public DateTime Vencimento { get; set; }
+
+        public void Pagar(double valor)
+        {
+            
+        }
+    }
+
+    public class Pagamento : IPayment
+    {
+        public DateTime Vencimento { get; set; }
+
+        public void Pagar(double valor)
+        {
+           
+        }
+    }
+}
