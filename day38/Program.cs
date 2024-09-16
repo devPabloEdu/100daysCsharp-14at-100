78238c8c93fe0f64e2f831b9ee8cde8febeef7f8
devpabloedu <vilelapege1113@gmail.com>
2024-09-16 20:54:29 -0300

    day38 - lists pt1

--- day38/Program.cs
+++ day38/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Payment> payments = new List<Payment>();
+            payments.Add(new Payment());
+
+            var lista = new List<string>();
+
+            Jogadores();
+        }
+
+        public static void Jogadores()
+        {
+            Console.Clear();
+            List<string> names = new List<string>();
+            names.Add("Talles Magno");
+            names.Add("Rodrigo Garro");
+            names.Add("Depay");
+            names.Add("Yuri Alberto");
+            names.Remove("Yuri Alberto");
+            names.Add("André Ramalho");
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Craque: {name.ToUpper()}");
+            }
+        }
+    }
+
+    public class Payment { }
+}
--- day38/obj/Debug/net8.0/day38.AssemblyInfo.cs
+++ day38/obj/Debug/net8.0/day38.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("100daysCsharp-14até100")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("day38")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52282460df511f8efff8ef72087e7c1428fceba2")]
-[assembly: System.Reflection.AssemblyProductAttribute("100daysCsharp-14até100")]
-[assembly: System.Reflection.AssemblyTitleAttribute("100daysCsharp-14até100")]
+[assembly: System.Reflection.AssemblyProductAttribute("day38")]
+[assembly: System.Reflection.AssemblyTitleAttribute("day38")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
