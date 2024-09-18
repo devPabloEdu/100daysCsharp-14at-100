c778858a94848164f9cc725d98a9e6b2cc15f8d9
devpabloedu <vilelapege1113@gmail.com>
2024-09-18 20:44:39 -0300

    day39 - Lists pt2 & 3

---
+++ day39/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            var payments = new List<Payment>();
+            payments.Add(new Payment(1));
+            payments.Add(new Payment(2));
+            payments.Add(new Payment(3));
+            payments.Add(new Payment(4));
+            payments.Add(new Payment(5));
+
+            foreach (var item in payments)
+            {
+                Console.WriteLine($"Listando : {item.Id}");
+            }
+
+            var payment = payments.First(x=>x.Id == 3);
+            Console.WriteLine(payment.Id);
+            var exists = payments.Any(x=>x.Id == 3);
+            Console.WriteLine(exists);
+
+            payments.Remove(payment);
+            foreach (var item in payments)
+            {
+                Console.WriteLine($"Listando : {item.Id}");
+            }
+            var exists2 = payments.Any(x=>x.Id == 3);
+            Console.WriteLine(exists2);
+            //APRENDENDO SOBRE O RANGE
+            //var paidPayments = new List<Payment>();
+            //paidPayments.AddRamge(payments);
+        }
+    }
+
+    public class Payment 
+    {
+        public int Id { get; set; }
+        public Payment(int id)
+        {
+            Id = id;
+        }
+     }
+}
--- day39/obj/Debug/net8.0/day39.AssemblyInfo.cs
+++ day39/obj/Debug/net8.0/day39.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("100daysCsharp-14até100")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("day39")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78238c8c93fe0f64e2f831b9ee8cde8febeef7f8")]
-[assembly: System.Reflection.AssemblyProductAttribute("100daysCsharp-14até100")]
-[assembly: System.Reflection.AssemblyTitleAttribute("100daysCsharp-14até100")]
+[assembly: System.Reflection.AssemblyProductAttribute("day39")]
+[assembly: System.Reflection.AssemblyTitleAttribute("day39")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
